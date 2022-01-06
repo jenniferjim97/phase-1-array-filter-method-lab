641cf31272d76ba4a0362db4e26c310a77a35a3f
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2022-01-06 12:03:09 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, driverName) {
+    return drivers.filter(function(name) {
+        return name.toLowerCase() === driverName.toLowerCase();
+    })
+
+}
+
+function fuzzyMatch(driverName, letterMatch){
+return driverName.filter(function(match){
+    return match.toLowerCase().indexOf(letterMatch.toLowerCase()) === 0
+})
+}
+
+function matchName(driver, driverName){
+    return driver.filter(driver => driver.name === driverName)
+
+}
